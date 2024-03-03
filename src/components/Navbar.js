40fe4339e4611b3f586d26fab70e1f8df126c0ad
OmidSargazi <om.sargazi@gmail.com>
2024-03-04 03:07:13 +0330

    create not found page with talwindcss

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className=" flex items-center justify-between h-[40px] md:h-[70px] bg-gray-700 w-full md:px-16 p-4 ">
+    <nav className=" flex text-white items-center justify-between h-[50px] md:h-[70px] bg-gray-700 w-full md:px-16 p-4 py-3 ">
       <Link to="/">
         <h2 className=" text-2xl md:text-4xl">Omid Online</h2>
       </Link>
@@ -12,7 +12,7 @@ const Navbar = () => {
         <Link to="/cart">
           <div className=" flex gap-3 items-center">
             <FaShoppingBag size={30} />
-            <span className=" text-xl md:text-3xl w-10 h-10 bg-yellow-400 rounded-full flex items-center justify-center ">
+            <span className=" text-xl md:text-3xl w-10 h-10 bg-yellow-400 text-gray-500 rounded-full flex items-center justify-center ">
               3
             </span>
           </div>
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className=" flex flex-col justify-center items-center h-screen w-full font-body">
+      <h2 className=" text-[40px] md:text-[70px] text-gray-500">404</h2>
+      <p className="text-[30px] md:text-[60px] text-gray-500">Page Not Found</p>
+    </div>
+  );
 };
 
 export default NotFound;
