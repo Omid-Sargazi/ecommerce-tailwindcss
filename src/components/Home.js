b1ecd3f5fedb366a02483668419df7a5ff6c412a
OmidSargazi <om.sargazi@gmail.com>
2024-03-06 03:11:16 +0330

    add products in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,38 @@ const Home = () => {
   const { data, error, isLoading } = useGetProductsApiByNameQuery();
   console.log(data, "data");
   return (
-    <div>
+    <div className=" py-8 px-16">
       {error ? (
         <>oh no, there was an error</>
       ) : isLoading ? (
         <>Loading...</>
       ) : data ? (
-        <>{}</>
+        <>
+          <h2 className=" text-[40px] text-center">New Arrivals</h2>
+          <div className=" flex justify-between flex-wrap mt-8">
+            {data?.map((product) => (
+              <div
+                className=" w-[250px] max-w-full h-[400px] flex flex-col justify-between mt-4 mb-4 p-4 shadow-xl rounded-xl"
+                key={product.id}
+              >
+                <h3 className=" text-2xl">{product.title.substring(0, 10)}</h3>
+                <img
+                  width="80%"
+                  className=" object-cover  h-[80%] mt-4 ml-auto mr-auto"
+                  src={product.image}
+                  alt={product.title.substring(0, 10)}
+                />
+                <div className=" flex items-center justify-between">
+                  <span>{product.description.substring(0, 15)}</span>
+                  <span className=" text-xl">${product.price}</span>
+                </div>
+                <button className=" w-full h-10 rounded-lg mt-8 cursor-pointer bg-[#4b70e2] hover:bg-[#5a67e0] text-white">
+                  Add to cart
+                </button>
+              </div>
+            ))}
+          </div>
+        </>
       ) : (
         ""
       )}
